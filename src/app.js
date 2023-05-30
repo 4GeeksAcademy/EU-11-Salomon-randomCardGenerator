1fa857bf9881ce644b7adbcb1bbb4e6e4a557083
Salomon Franco <salomonfrancodasilva@gmail.com>
2023-05-30 23:15:01 +0100

    structureDesign&ScriptUpdate
    The structure of the card generator is made using html, css and bootstrap 5.
    The Script is updated with all the information necessary but still needs to be enhanced to be properly working.

--- src/app.js
+++ src/app.js
@@ -7,5 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function getRandomElementFromArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+  window.addEventListener("load", () => {
+    const suits = ["♦", "♥", "♠", "♣"];
+    const numbers = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "King",
+      "Queen",
+      "Jack",
+      "Ace"
+    ];
+    const randomSuit = getRandomElementFromArray(suits);
+    const randomNumber = getRandomElementFromArray(numbers);
+    const suitElements = document.querySelectorAll(".suit-element");
+    suitElements.forEach(e => {
+      e.innerHTML = randomSuit;
+    });
+    const numberElement = document.querySelector(".number-element");
+    numberElement.innerHTML = randomNumber;
+  });
 };
