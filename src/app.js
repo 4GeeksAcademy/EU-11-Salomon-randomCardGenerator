8b7d11e2742effb25b20a0523ce5124a972f1388
Salomon Franco <salomonfrancodasilva@gmail.com>
2023-06-01 05:20:50 +0100

    scriptButton&SizeFunctionAdded

--- src/app.js
+++ src/app.js
@@ -5,35 +5,75 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function restartPage() {
+  location.reload();
+}
+
 window.onload = function() {
-  //write your code here
-  function getRandomElementFromArray(array) {
-    return array[Math.floor(Math.random() * array.length)];
-  }
-  window.addEventListener("load", () => {
-    const suits = ["♦", "♥", "♠", "♣"];
-    const numbers = [
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "King",
-      "Queen",
-      "Jack",
-      "Ace"
-    ];
-    const randomSuit = getRandomElementFromArray(suits);
-    const randomNumber = getRandomElementFromArray(numbers);
-    const suitElements = document.querySelectorAll(".suit-element");
-    suitElements.forEach(e => {
-      e.innerHTML = randomSuit;
-    });
-    const numberElement = document.querySelector(".number-element");
-    numberElement.innerHTML = randomNumber;
-  });
+  cardGenerator();
 };
+
+function cardGenerator() {
+  const suits = ["♦", "♥", "♠", "♣"];
+  const numbers = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  const cardSuiteHeader = document.querySelector(".card-header");
+  const cardNumber = document.querySelector(".card-text");
+  const cardSuiteFooter = document.querySelector(".card-footer");
+  const cardWidthInput = document.getElementById("cardWidth");
+  const cardHeightInput = document.getElementById("cardHeight");
+  const applyButton = document.getElementById("applyButton");
+
+  applyButton.addEventListener("click", applyChanges);
+
+  function applyChanges() {
+    const cardWidth = cardWidthInput.value;
+    const cardHeight = cardHeightInput.value;
+    const card = document.querySelector(".card");
+
+    if (cardWidth && cardHeight) {
+      card.style.maxWidth = `${cardWidth}px`;
+      card.style.height = `${cardHeight}px`;
+    }
+  }
+
+  setInterval(() => {
+    location.reload();
+  }, 10000);
+
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+  cardSuiteHeader.innerHTML = randomSuit;
+  cardNumber.innerHTML = randomNumber;
+  cardSuiteFooter.innerHTML = randomSuit;
+
+  if (randomSuit === "♦" || randomSuit === "♥") {
+    cardSuiteHeader.classList.add("red");
+    cardNumber.classList.add("red");
+    cardSuiteFooter.classList.add("red");
+  }
+
+  var reloadButton = document.getElementById("reloadButton");
+
+  function handleReload() {
+    location.reload();
+  }
+
+  reloadButton.addEventListener("click", handleReload);
+}
